d5b9aac6ded455b0b658f6195e8fc020235ad725
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-08-20 14:09:59 +0300

    more nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,7 +5,13 @@ export default function Nav() {
     return (
       <nav className={css.nav}>
         <NavLink className={css.find} to="/notices">
+          News
+        </NavLink>
+        <NavLink className={css.find} to="/notices">
           Find pet
+        </NavLink>
+        <NavLink className={css.find} to="/notices">
+         Our friends
         </NavLink>
       </nav>
     );
