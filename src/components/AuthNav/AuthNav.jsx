3441df7d2c945c6e4685db04c38861061c773f6e
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-08-20 14:14:51 +0300

    start auth

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,3 +1,17 @@
+import { NavLink } from "react-router-dom";
+import css from "./AuthNav.module.css"
+
 export default function AuthNav() {
-    return <></>
+    return (
+      
+        <div>
+          <NavLink className={css.link} to="/register">
+            Register
+          </NavLink>
+          <NavLink className={css.link} to="/login">
+            Log In
+          </NavLink>
+        </div>
+      
+    );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,15 @@ import css from './Header.module.css'
 import Nav from 'components/Nav/Nav';
 import Logo from 'components/Logo/Logo';
 import React from 'react';
+import AuthNav from 'components/AuthNav/AuthNav';
+
 export default function Header() {
     return (
   
         <header className={css.header}>
           <Logo />
-          <Nav />
+        <Nav />
+        <AuthNav/>
         </header>
       
     );
