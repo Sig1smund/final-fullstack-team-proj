b69c466703808b36c92345256c60270bb502656b
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-08-22 22:38:06 +0300

    MainPage без вірного шрифта

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
+import MainPage from '../pages/MainPage'
+
 export const App = () => {
   return (
-    <div>
-      Hello programmers
-    </div>
+    // <div>
+    //   Hello programmers
+    // </div>
+    <MainPage/>
   );
 };
