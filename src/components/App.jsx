8de051627eef2b58a776c432a5ac7d65796522ee
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-08-23 19:00:43 +0300

    NoticesSearch без шрифтів

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import s from './app.module.css'
 
 export const App = () => {
-  return (
+  return ( <>
     <div className={s.container}>
       <input type="text" className={s.input} />
       <button type='button' className={s.button}>To your IT life</button>
     </div>
+      </>
   );
 };
--- src/components/NoticesSearch/NoticesSearch.jsx
+++ src/components/NoticesSearch/NoticesSearch.jsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+import styles from './noticesSearch.module.css';
+import sprite from '../../images/sprite.svg'
+
 export default function NoticesSearch() {
-    return <></>
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(name)
+
+        setName('');
+  }
+
+    return <>
+    <form className={styles.form} onSubmit={handleSubmit}>      
+          <input
+            className={styles.input}
+            onChange={(e) => setName(e.currentTarget.value)}
+            value={name}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search"
+          />
+        <button type="submit" className={styles.button}>
+        <svg width='24px' height='24px' className={styles.iconSearch}>
+            <use href={sprite + "#search"}></use>
+        </svg>
+          </button>
+          {name !== '' && (<button type="button" className={styles.button} onClick={() => {setName('')}}>
+        <svg className={styles.iconClose} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M19 5L5 19M5.00004 5L19 19" stroke="#FFC107" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        </button>)}
+    </form>
+    </>
 }
