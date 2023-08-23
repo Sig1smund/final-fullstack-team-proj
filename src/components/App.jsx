665602918248c0155689123eb38ec5d2cd6205cb
Nadiia Maltseva <amanavi2021@gmail.com>
2023-08-24 00:26:36 +0300

    update private route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,31 @@ import { Route, Routes } from 'react-router-dom';
 // import s from './app.module.css';
 
 import  SharedLayout from '../components/SharedLayout';
+import { RestrictedRoute } from './RedirectedRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 const HomePage = lazy(()=> import('../pages/MainPage'));
-// const RegisterPage = lazy(()=> import('../pages/RegisterPage'));
-// const LoginPage = lazy(()=> import('../pages/LoginPage'));
+const RegisterPage = lazy(()=> import('../pages/RegisterPage'));
+const LoginPage = lazy(()=> import('../pages/LoginPage'));
 // const UserPage = lazy(()=> import('../pages/UserPage'));
-// const NewsPage = lazy(()=> import('../pages/NewsPage'));
+ const NewsPage = lazy(()=> import('../pages/NewsPage'));
 // const NoticesPage = lazy(()=> import('../pages/NoticesPage'));
-// const OurFriendPage = lazy(()=> import('../pages/OurFriendsPage'));
+ const OurFriendPage = lazy(()=> import('../pages/OurFriendsPage'));
 
 export const App = () => {
   return (
     <Routes>
       <Route path='/' element={<SharedLayout/>}> 
-      <Route index element={<HomePage/>}/>          
+        <Route index element={<HomePage/>}/>     
+        <Route path="/register" element= {
+        <RestrictedRoute redirectTo='/' component={<RegisterPage/>}/>
+        }/>
+        <Route path="/login" element= {
+        <RestrictedRoute redirectTo='/' component={<LoginPage/>}/>
+        }/>
+        <Route path='/news' element={<NewsPage/>}/>
+        <Route path='/friends' element={<OurFriendPage/>}/>
+
       </Route>
           
 
