17818cb35ba95de4c114c74cff5ff469c2371827
lili2628 <113315152+lili2628@users.noreply.github.com>
2023-08-24 08:16:55 +0000

    Deploying to gh-pages from @ Sig1smund/final-fullstack-team-proj@120914b1f40f351a0e7b10b14e4984eeeec68482 🚀

--- static/js/986.e754b5f9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfinal_fullstack_team_proj=self.webpackChunkfinal_fullstack_team_proj||[]).push([[986],{986:function(a,e,s){s.r(e),s.d(e,{default:function(){return i}});s(791);var n="mainPage_container__zqE06",l="mainPage_petsImg__3BC-h",t="mainPage_title__FDopH",c=s(329);function i(){return(0,c.jsxs)("div",{className:n,children:[(0,c.jsx)("h1",{className:t,children:"Take good care of your small pets"}),(0,c.jsx)("div",{className:l})]})}}}]);
-//# sourceMappingURL=986.e754b5f9.chunk.js.map
