9593f5c899873e9301af8901a2c32856641b95a1
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-08-25 13:46:04 +0300

    NoticesPage

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,9 +1,20 @@
 import NoticesSearch from '../../components/NoticesSearch'
+import NoticesCategoriesNav from '../../components/NoticesCategoriesNav'
+import NoticesFilters from '../../components/NoticesFilters'
+import NoticesCategoriesList from '../../components/NoticesCategoriesList'
+import AddPetForm from '../../components/AddPetForm'
+import useAuth from 'hooks';
 import styles from './NoticesPage.module.css'
 
 export default function NoticesPage() {
+    const { isLoggedIn } = useAuth();
+
     return <div className={styles.container}>
     <h1 className={styles.title}>Find your favorite pet</h1>
     <NoticesSearch/>
+    <NoticesCategoriesNav/>
+    <NoticesFilters/>
+    {isLoggedIn && <AddPetForm/>}
+    <NoticesCategoriesList/>
     </div>
 }
