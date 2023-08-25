7e9a25e7b5f0e659f4414e3e457ca77ac63af251
Iryna Lazareva <il337733@gmail.com>
2023-08-25 19:33:50 +0300

    change

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -2,31 +2,18 @@ import { useState } from 'react';
 import css from './UserPage.module.css'
 import sprite from '../../images/sprite.svg'
 import UserForm from 'components/UserForm/UserForm'
-import PetsList from 'components/PetsList/PetsList'
-// import UserForm from 'components/UserForm/UserForm';
 import Cross from '../../images/cross-small.svg'
-
-// import { useDispatch } from 'react-redux';
-// import { current } from 'redux/auth/operations';
-
 import { useSelector } from 'react-redux';
 import {selectUser} from '../../redux/auth/selectors'
+import PetsData from 'components/PetsData/PetsData';
 
 
 export default function UserPage() {
 const [readOnly, setReadOnly]=useState(true)
-// const [user, setUser]=useState('')
+
 
 const user = useSelector(selectUser)
 
-// const user ={
-//     "name": "iii",
-//     "email": "iiii@mail.com"
-// }
-// const dispatch = useDispatch();
-// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZTcxYmJlNmZiNzViZDcyYWU4MTJlNyIsImlhdCI6MTY5Mjg2NzU5NCwiZXhwIjoxNjkyODc0Nzk0fQ.hZRsX9xr_ZVU1M3GHVVqhdKx3GNI-57dDWxTeVeKVj0'
-// const user = current(token)
-// console.log(user)
 
 
 
@@ -67,11 +54,8 @@ const onToggleReadOnly = () => {
 {readOnly ? (<UserForm readonly={true} user={user}/>):(<UserForm readonly={false} user={user} saveNewPhoto={saveNewPhoto} onSubmit={onSubmitForm} />)}
 
 </div>
-<div className={css.petsListWrapper}>
-    <h2 className={css.title}>My pets:</h2>
-    <PetsList/>
-</div>
 
+<PetsData/>
 
     </div>
 
