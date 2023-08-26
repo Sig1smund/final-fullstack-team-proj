e86cd11178bdc8ce9f0e2d92c21c04dd14293bce
Iryna Lazareva <il337733@gmail.com>
2023-08-26 14:41:14 +0300

    change

--- src/components/PetsItem/PetsItem.jsx
+++ src/components/PetsItem/PetsItem.jsx
@@ -1,18 +1,43 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {deletePet} from '../../redux/auth/operations'
 import css from './PetsItem.module.css'
 import sprite from '../../images/sprite.svg'
+import ModalApproveAction from 'components/ModalApproveAction/ModalApproveAction';
+import Modal from 'components/Modal/Modal';
+
 export default function PetsItem({pet}) {
+
     const [showModal, setShowModal] = useState(false);
-   console.log("pet", pet);
+
+   console.log(showModal);
+const dispatch = useDispatch();
+
+const openModal = () => {
+    setShowModal(true);
+   
+  };
+
+const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const approveAction = () => {
+    dispatch(deletePet(pet._id));
+    console.log(pet._id)
+    setShowModal(false);
+  };
+
     return (
         <div className={css.petsListWrapper}>
         <li className={css.petsItemWrapper}>
 <img src={pet.imageURL} alt={pet.name} className={css.petsImg} />
 <div className={css.petInfoWrapper}>
     <p className={css.petInfoBtn}>Name: {pet.name}
+    
     <button
-    type='submit'
-    onClick={setShowModal}
+    type='button'
+    onClick={openModal}
     className={css.btnTrash}
     >
         <svg  width="24" height="24"  >
@@ -22,12 +47,20 @@ export default function PetsItem({pet}) {
     </button>
     
     </p>
-    <p className={css.petInfo}><b>Date of birth:</b> {pet.birthday}</p>
+    <p className={css.petInfo}><b>Date of birth:</b> {pet.date}</p>
     <p className={css.petInfo}><b>Type:</b> {pet.type}</p>
     <p className={css.petInfo}><b>Comments: </b>{pet.comments}</p>
 </div>
 </li>
-{showModal && (<p>Тут буде модалка</p>)}
+{showModal && (
+    <Modal isOpen={openModal} onClose={closeModal}> 
+<ModalApproveAction 
+close={closeModal} 
+action={approveAction} > 
+Are you sure you want to delete  “Cute dog looking for a home”? 
+You can`t undo this action.
+</ModalApproveAction>
+</Modal>)}
 </div>
 
     )
