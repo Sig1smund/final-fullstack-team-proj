834596f9e8edef68ea92b292d3ef19cab2151a7f
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2023-08-26 03:59:14 +0300

    redux

---
+++ src/redux/notices/operations.js
@@ -0,0 +1,96 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// axios.defaults.baseURL = 'https://team-project-pets-backend.onrender.com/';
+
+// const setAuthHeader = token => {
+//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+// };
+
+// const clearAuthHeader = () => {
+//   axios.defaults.headers.common.Authorization = '';
+// };
+
+export const getNotices = createAsyncThunk(
+  'notices/getNotices',
+  async (category, thunkAPI) => {
+    try {
+      const res = await axios.get('api/notices', category);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addNotice = createAsyncThunk(
+  'notices/addNotice',
+  async (notice, thunkAPI) => {
+    try {
+      const res = await axios.post('api/notices', notice);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const getOwnNotices = createAsyncThunk(
+  'notices/getOwnNotices',
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios.get('api/notices/own');
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const getFavNotices = createAsyncThunk(
+  'notices/getFavNotices',
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios.get('api/notices/favorites');
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const getNotice = createAsyncThunk(
+  'notices/getNotice',
+  async (noticeId, thunkAPI) => {
+    try {
+      const res = await axios.get(`api/notices/${noticeId}`);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const setFavNotice = createAsyncThunk(
+  'notices/setFavNotice',
+  async (noticeId, thunkAPI) => {
+    try {
+      const res = await axios.patch(`api/notices/${noticeId}`);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const removeFavNotice = createAsyncThunk(
+  'notices/removeFavNotice',
+  async (noticeId, thunkAPI) => {
+    try {
+      const res = await axios.delete(`api/notices/${noticeId}`);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
