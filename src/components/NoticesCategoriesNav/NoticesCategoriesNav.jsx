043652fb106c707014ef61606c67f789f8705fc7
Nadiia Maltseva <amanavi2021@gmail.com>
2023-08-27 11:47:58 +0300

    fix notices categoriNav

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -4,15 +4,25 @@ import styles from './NoticesCategoriesNav.module.css'
 export default function NoticesCategoriesNav() {
     return (
         <nav className={styles.container}>
-        <NavLink activeClassName={styles.active} className={styles.button} to="/notices/sell">
+        <NavLink className={styles.button} to="/notices/sell">
         sell
         </NavLink>
-        <NavLink activeClassName={styles.activeClassName} className={styles.button} to="/notices/lost-found">
+        <NavLink className={styles.button} to="/notices/lost-found">
+        lost/found
+        </NavLink>
+        <NavLink className={styles.button} to="/notices/in-good-hands">
+        in-good-hands
+        </NavLink>
+
+        {/* <NavLink activeClassName={styles.active} className={styles.button} to="/notices/sell">
+        sell
+        </NavLink>
+        <NavLink activeClassName={styles.active} className={styles.button} to="/notices/lost-found">
         lost/found
         </NavLink>
         <NavLink activeClassName={styles.active} className={styles.button} to="/notices/in-good-hands">
         in-good-hands
-        </NavLink>
+        </NavLink> */}
       </nav>
     )
 }
