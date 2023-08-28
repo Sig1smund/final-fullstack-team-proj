de0c6f3cca3911c1768114e2f65267f814f49669
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-08-28 17:59:44 +0300

    update

--- src/components/NoticeModal/NoticeModal.jsx
+++ src/components/NoticeModal/NoticeModal.jsx
@@ -1,75 +1,87 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { toast } from 'react-toastify';
+//import { useState } from "react";
+//import { toast } from 'react-toastify';
 
-import useAuth from "hooks/useAuth";
-import { getNotice, setFavNotice } from "redux/notices/operations";
+//import useAuth from "hooks/useAuth";
+//import { getNotice, setFavNotice } from "redux/notices/operations";
 
 import css from "./NoticeModal.module.css";
 import icon from '../../images/sprite.svg';
-import Modal from "components/Modal";
-import AttentionModal from "components/AttentionModal";
+//import Modal from "components/Modal";
+//import AttentionModal from "components/AttentionModal";
 
 
-export default function NoticeModal({ onClose, id, isFavorite }) {
-  const { isLoggedIn } = useAuth();
+export default function NoticeModal({ id, isFavorite }) {
+  //const { isLoggedIn } = useAuth();
+  const isLoggedIn = true;
 
-  const { data } = getNotice(id);
+  //const { data } = getNotice(id);
 
-  if (!data) {
-    return;
-  };
+ // if (!data) {
+  //  return;
+  //};
 
-  const { notice } = data;
+  //const { notice } = data;
 
-  const {
-    title,
-    imageURL,
-    category,
-    name,
-    type,
-    birthday,
-    sex,
-    location,
-    price,
-    comments,
-    owner.email,
-    owner.phone,
-  } = notice;
+  const title = "Sell Cat Felis chaus";
+  const imageURL = "https://upload.wikimedia.org/wikipedia/commons/8/82/Jungle_Cat_on_tree_at_Sundarban%2C_West_Bengal%2C_India.jpg";
+  const category = "sell";
+  const name = "Nick";
+  const type = "cat";
+  const birthday = "26.09.2018";
+  const sex = "male";
+  const location = "Kyiv";
+  const price = 10;
+  const comments = "gggggggggggg";
+  const phone = "+380665214154";
+  const email = "lili262808@gmail.com";
 
-  const phoneNumber = `tel:${phone}`;
-  const emailAddress = `mailto:${email}`;
+  //const {
+  //  title,
+   // imageURL,
+  //  category,
+  //  name,
+   // type,
+   // birthday,
+  //  sex,
+  //  location,
+   // price,
+  //  comments,
+   // owner,
+ // } = notice;
 
-  const showPrice = category === 'sell' ? true : false;
+ // const { email, phone } = owner;
 
-  const [isAdditionModalOpen, setIsAdditionModalOpen] = useState(false);
+ const phoneNumber = `tel:${phone}`;
+ const emailAddress = `mailto:${email}`;
+
+ // const showPrice = category === 'sell' ? true : false;
+  const showPrice = true;
+
+ //const [isAdditionModalOpen, setIsAdditionModalOpen] = useState(false);
 
   const onFavoriteBtnClick = (id) => {
     if (isLoggedIn) {
       try {
-        setFavNotice(id);
+       // setFavNotice(id);
       } catch (error) {
         console.log(error);
-      }
-    } else {
-      setIsAdditionModalOpen(true);
+      };
     };
+  //  setIsAdditionModalOpen(true);
   };
 
-  const close = () => {
-    setIsAdditionModalOpen(false);
-  };
+ // const close = () => {
+ //   setIsAdditionModalOpen(false);
+ // };
 
-  const onContactBtnClick = (phone) => {
-    toast.error('Please use the email for contact');
-  };
 
   return (
     <>
       <div className="container">
       <div className="noticeCard">
         <label className="category">{category}</label>
-        <img className="photo" src={imageURL} />
+        <img alt="notice pet avatar" className="photo" src={imageURL} />
 
         <div className="information">
           <h2 className="title">{title}</h2>
@@ -95,12 +107,12 @@ export default function NoticeModal({ onClose, id, isFavorite }) {
           </div>
           <div className="informationItem">
             <span className="key">Email:</span>
-            <Link className="value" href={emailAddress}>{ owner.email }</Li>
+            <Link className="value" href={emailAddress}>{ email }</Link>
           </div>
           {phone !== '' && (
             <div className="informationItem">
               <span className="key">Phone:</span>
-              <Link className="value" href={phoneNumber}>{ owner.phone }</Link>
+              <Link className="valueLink" href={phoneNumber}>{ phone }</Link>
             </div>
           )}
           {showPrice && (
@@ -111,37 +123,37 @@ export default function NoticeModal({ onClose, id, isFavorite }) {
           )}
         </div>
 
-        <div className="commentContainer">
+          <div className={css.commentContainer}>
           <span className="key">Comments:</span>
           <p className="value">{comments}</p>
         </div>
       </div>
 
-      <button className="favoriteBtn" onClick={() => onFavoriteBtnClick(id)} isFavorite={isFavorite}>
+        <button className={css.favoriteBtn} onClick={() => onFavoriteBtnClick(id)} isFavorite={isFavorite}>
         {isFavorite ? 'Remove from' : 'Add to'}
         <svg className={css.heart} width="24" height="24">
-          <use href={icon + '#heart'}></use>
+            <use className={css.iconHeart} href={icon + '#heart'}></use>
         </svg>
       </button>
 
-      <div className="contactBtnContainer">
+        <div className={css.contactBtnContainer}>
         {phone === '' ? (
-          <button type="button" className="contactBtn" onClick={onContactBtnClick}>
+            <Link className={css.contactLinkMail} to={emailAddress}>
             Contact
-          </button>
+          </Link>
         ) : (
-          <Link className="contactLink" to={phoneNumber}>
+              <Link className={css.contactLinkPhone} to={phoneNumber}>
             Contact
           </Link>
         )}
       </div>
       </div>
 
-      {isAdditionModalOpen && (
-        <Modal isOpen={isAdditionModalOpen} onClose={close}>
-          <AttentionModal />
+    { /* isAdditionModalOpen && (
+       <Modal isOpen={isAdditionModalOpen} onClose={close}>
+         <AttentionModal />
         </Modal>
-      )}
+      )*/}
     </>
 
   );
