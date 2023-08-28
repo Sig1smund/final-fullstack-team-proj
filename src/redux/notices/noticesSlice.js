ef042491697bdc0ab79f33bc415e2c60cb877776
Nadiia Maltseva <amanavi2021@gmail.com>
2023-08-29 01:43:25 +0300

    fix delete notice

--- src/redux/notices/noticesSlice.js
+++ src/redux/notices/noticesSlice.js
@@ -90,8 +90,8 @@ const noticesSlice = createSlice({
         state.isLoading = true;
       })
       .addCase(removeOwnNotice.fulfilled, (state, action) => {
-        console.log('action.payload', action.payload)
-        state.notices = state.notices.filter(n => n._id !== action.payload._id);
+        console.log('action.payload', action.payload);
+        state.notices = state.notices.filter(n => n._id !== action.payload.id);
         state.isLoading = false;
       })
       .addCase(removeOwnNotice.rejected, (state, action) => {
