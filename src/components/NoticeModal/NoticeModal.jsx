0a132275ac8475a70b54a7a2e9371ad7efbc1226
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-08-30 23:59:14 +0300

    fixed close modal

--- src/components/NoticeModal/NoticeModal.jsx
+++ src/components/NoticeModal/NoticeModal.jsx
@@ -44,14 +44,6 @@ export default function NoticeModal({ item, handler, favorites }) {
     return console.log(id);
   }
 
-  const close = () => {
-    setIsAttentionModalOpen(false);
-  };
-
-  const toggleModal = () => {
-    setIsAttentionModalOpen(prev => !prev)
-    console.log('fire');
-  }
 
 
 
