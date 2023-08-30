2143aa87a076b17e00dababeabb23cf1be22aa51
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-08-31 00:00:48 +0300

    fix logout in burger and username in mobile burger

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -2,18 +2,44 @@ import React, { useState } from 'react';
 import css from './BurgerMenu.module.css';
 import logo from '../../images/logo.svg';
 import svg from '../../images/sprite.svg';
-import  useAuth  from 'hooks';
+import useAuth from 'hooks';
 import logout from '../../images/sprite.svg';
 import { NavLink } from 'react-router-dom';
 import pawprint from '../../images/sprite.svg';
-import user_svg from '../../images/sprite.svg'; 
+import user_svg from '../../images/sprite.svg';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Modal from 'components/Modal/Modal';
+import ModalApproveAction from 'components/ModalApproveAction/ModalApproveAction';
+import { logOut } from 'redux/auth/operations';
 
 const BurgerMenu = () => {
+   const { user } = useAuth();
   const [nav, setNav] = useState(false);
   const { isLoggedIn } = useAuth();
-   const handleMenuClick = () => {
-     setNav(!nav);
-   };
+  const handleMenuClick = () => {
+    setNav(!nav);
+  };
+  const navigate = useNavigate();
+
+  const [modalActive, setModalActive] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const openModal = () => {
+    setModalActive(true);
+  };
+
+  const close = () => {
+    setModalActive(false);
+  };
+
+  const userLogout = async () => {
+    dispatch(logOut());
+    setModalActive(false);
+    navigate('/user');
+  };
+
   return (
     <>
       <div
@@ -26,20 +52,30 @@ const BurgerMenu = () => {
 
         {isLoggedIn ? (
           <>
-            <button className={css.btn} type="button">
+            <button className={css.btn} type="button" onClick={openModal}>
               Log out
               <svg className={css.logout} width="24" height="24">
                 <use href={logout + '#logout'}></use>
               </svg>
             </button>
+
+            <Modal isOpen={modalActive} onClose={close}>
+              <ModalApproveAction
+                logo={'#logout'}
+                close={close}
+                action={userLogout}
+              >
+                Already leaving?
+              </ModalApproveAction>
+            </Modal>
             <div className={css.thumb_auth}>
               <NavLink to="/user" className={css.profile}>
                 <svg className={css.icon__user} width="28" height="28">
                   <use href={user_svg + '#user-1'}></use>
                 </svg>
-                <span className={css.username}>Anna</span>
+                <span className={css.username}>{user.name}</span>
               </NavLink>
-            </div>{' '}
+            </div>
           </>
         ) : (
           <div className={css.auth__container}>
