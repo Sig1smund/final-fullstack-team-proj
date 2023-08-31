85840759ba2f15f4179fae491818d3e48292409b
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2023-08-31 11:25:22 +0300

    added default pic

--- src/components/OurFriendsList/OurFriendsList.jsx
+++ src/components/OurFriendsList/OurFriendsList.jsx
@@ -1,5 +1,6 @@
 import useServices from "hooks/useServices";
-import s from './OurFriendsList.module.css'
+import s from './OurFriendsList.module.css';
+const defaultImg = 'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=158x150';
 
 export default function OurFriendsList() {
     const { services } = useServices();
@@ -14,17 +15,17 @@ export default function OurFriendsList() {
                             <div className={s.info}>
                                 <div className={s.thumb}>
                                     <a href={service.url} target="_blank" rel="noreferrer">
-                                        <img src={service.imageUrl} alt={service.title} className={s.image} />
+                                        <img src={service.imageUrl || defaultImg} alt={service.title} className={s.image} />
                                     </a>
                                 </div>
                                 <div className={s.details}>
-                                    <p>Time</p>
-                                    <p>{service.workdays}{'-'}{service.workdays}</p>
-                                    <p>Adress</p>
-                                    <p>{service.adress}</p>
-                                    <p>Email</p>
+                                    <p className={s.infoTitle}>Time</p>
+                                    <p className={s.infoDetails}>{service.workdays}{'-'}{service.workdays}</p>
+                                    <p className={s.infoTitle}>Adress</p>
+                                    <p className={s.infoDetails}>{service.adress || 'website only'}</p>
+                                    <p className={s.infoTitle}>Email</p>
                                     <a href="mailto:">{service.email || 'phone only'}</a>
-                                    <p>Phone</p>
+                                    <p className={s.infoTitle}>Phone</p>
                                     <a href='tel:'>{service.phone || 'email only'}</a>
                                 </div>
                             </div>
