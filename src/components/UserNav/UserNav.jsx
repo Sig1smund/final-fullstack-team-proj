661633603b7fa086b3bcd5e469cbc4f3000372d9
Nadiia Maltseva <amanavi2021@gmail.com>
2023-09-01 08:15:05 +0300

    adds avatar

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -7,14 +7,21 @@ import  useAuth  from 'hooks';
 
 export default function UserNav() {
 
-   const { user } = useAuth();
+  const { user } = useAuth();
+  const { avatarURL } = user;
     return (
       <div className={css.thumb_auth}>
         <Logout />
         <NavLink to="/user" className={css.profile}>
-          <svg className={css.icon__user} width="28" height="28">
-            <use href={user_svg + '#user-1'}></use>
-          </svg>
+          {avatarURL ? <img
+            src={avatarURL}
+            className={css.avatar__user}
+            alt="profile"
+          /> :
+            <svg className={css.icon__user} width="28" height="28">
+              <use href={user_svg + '#user-1'}></use>
+            </svg>
+          }
           <span className={css.username}>{user.name}</span>
         </NavLink>
       </div>
